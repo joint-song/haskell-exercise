b4d0d044e45a873ee8ea6feb927738ac134b6ece
joint-song <xuguang1992@vip.qq.com>
2016-12-16 00:28:37 +0800

    rename function.

--- problem 1-10/p-01.hs
+++ problem 1-10/p-01.hs
@@ -8,20 +8,20 @@
 -- 'z'
 
 -- solution 0 (not well.)
-myLast :: [a] -> a
-myLast xs = if (null xs) || (length xs == 0)
+myLast' :: [a] -> a
+myLast' xs = if (null xs) || (length xs == 0)
                 then error "invalid input"
                 else xs !! ((length xs) - 1)
 
--- solution 1 (better one)
-myLast :: [a] -> a
-myLast [] = error "empty list error."
-myLast (x:[]) = x
-myLast xs = myLast (tail xs)
+-- solution 1 (better)
+myLast'' :: [a] -> a
+myLast'' [] = error "empty list error."
+myLast'' (x:[]) = x
+myLast'' xs = myLast (tail xs)
 
 -- solution 2
-myLast2 :: [a] -> a
-myLast2 [] = error "empty list error."
-myLast2 (x:xs) = if (length xs == 0)
+myLast''' :: [a] -> a
+myLast''' [] = error "empty list error."
+myLast''' (x:xs) = if (length xs == 0)
     then x
     else myLast2 xs
