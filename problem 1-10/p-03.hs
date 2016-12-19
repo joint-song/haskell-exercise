52fce75ded62840e9534d2f0edd9798ab75353b8
joint-song <xuguang1992@vip.qq.com>
2016-12-20 00:11:32 +0800

    03

---
+++ problem 1-10/p-03.hs
@@ -0,0 +1,15 @@
+-- Find the K'th element of a list. The first element in the list is number 1.
+
+-- Example in Haskell:
+
+-- Prelude> elementAt [1,2,3] 2
+-- 2
+-- Prelude> elementAt "haskell" 5
+-- 'e'
+
+elementAt :: (Num i, Ord i) => [a] -> i -> a
+elementAt [] _ = error "empty list error or index is out of range"
+elementAt (x:xs) i
+    | i <= 0 = error "index should be positive integer or element is not found"
+    | i == 1 = x
+    | otherwise = elementAt xs (i-1)
