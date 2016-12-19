db76ff3a1adcb722443e471115e356ee52f268f7
joint-song <xuguang1992@vip.qq.com>
2016-12-20 00:21:13 +0800

    04

---
+++ problem 1-10/p-04.hs
@@ -0,0 +1,12 @@
+-- Find the number of elements of a list.
+
+-- Example in Haskell:
+
+-- Prelude> myLength [123, 456, 789]
+-- 3
+-- Prelude> myLength "Hello, world!"
+-- 13
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
