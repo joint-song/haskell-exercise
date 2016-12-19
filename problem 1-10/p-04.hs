075a1c3c2461feaf128c52949ba41dbadeb827d5
joint-song <xuguang1992@vip.qq.com>
2016-12-20 00:30:07 +0800

    04

--- problem 1-10/p-04.hs
+++ problem 1-10/p-04.hs
@@ -7,6 +7,15 @@
 -- Prelude> myLength "Hello, world!"
 -- 13
 
+-- solution 0:
 myLength :: [a] -> Int
 myLength [] = 0
 myLength (_:xs) = 1 + myLength xs
+
+-- solution 1:
+-- From: https://wiki.haskell.org/99_questions/Solutions/4
+myLength' :: [a] -> Int
+myLength' list = acc list 0
+    where   
+        acc [] n = n
+        acc (_:xs) n = acc xs (n + 1)
